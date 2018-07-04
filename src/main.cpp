39ca2f88ee745ff6932da6ee17bc81af077234df
njzwj <njzwj2007@126.com>
2018-07-04 11:51:32 +0800

    Add license. Demo modification.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
+/*
+Copyright 2018 njzwj. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software 
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+*/
+
 #include <cstdio>
 #include <cstdlib>
 #include <Windows.h>
 #include "cli_graph.h"
-#include "svpng.h"
+#include "svpng.inc"
 #include "pixpix.h"
 using namespace pixpix;
 using namespace std;
@@ -146,28 +168,15 @@ int main() {
         lgt->mPosition = {-1.0f, -5.0f, 5.0f};
         pipeline->addLight(*lgt);
 
-        tex->color1 = {0.2, 0.9, 0.1, 1.0};
-        tex->color2 = {0.0, 0.1, 0.1, 1.0};
+        tex->color1 = {0.2, 1.0, 0.5, 1.0};
+        tex->color2 = {0.0, 0.5, 1.0, 1.0};
         pipeline->setTexture(tex);
         drawSphere(pipeline, 1.0f, 12, 6, {0,0,0}, {-Math::Pi/30.0f*i,Math::Pi/60.0f*i,Math::Pi/76.0f*i});
-        tex->color1 = {0.0, 0.2, 0.8, 1.0};
-        tex->color2 = {0.0, 0.0, 0.0, 1.0};
+        tex->color1 = {1.0, 0.5, 0.5, 1.0};
+        tex->color2 = {0.1, 0.1, 0.1, 1.0};
         pipeline->setTexture(tex);
         drawPlane(pipeline, 3.0f, 3.0f, {0.0f,0.0f,0.0f}, {0, 0, 0});
-        // 
-        // tex->color1 = {0.1, 0.3, 0.9, 1.0};
-        // tex->color2 = {0.0, 0.1, 0.3, 1.0};
-        // pipeline->setTexture(tex);
-        // drawPlane(pipeline, 2.0f, 2.0f, {0.0f,0.0f,0.0f}, {-Math::Pi/180.0f*3.0f*i+1.4f, 0, Math::Pi/180.0f*3.0f*i-0.3f});
 
-        // tex->color1 = {1.0, 0.0, 0.0, 1.0};
-        // tex->color2 = {0.0, 0.0, 1.0, 1.0};
-        // pipeline->setTexture(tex);
-        // drawPlane(pipeline, 2.0f, 2.0f, {0.0f,0.0f,0.0f}, {-Math::Pi/160.0f*i+1.5f, Math::Pi/95.0f*i, -Math::Pi/304.0f*i});
-        
-        // FILE *fp = fopen("./test.png", "wb");
-        // svpng(fp, W, H, cav->img, 0);
-        // fclose(fp);
         cli_graph(W, H, cav->img);
         Sleep(50);
     }
